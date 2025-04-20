33a2843fa8b7b5b177e84998aa746fe8d60987fe
LS2603 <liam.snowball@eque2.com>
2025-04-20 15:29:11 +0100

    add initial styling

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,12 +1,13 @@
-export default function SearchResults( {tracks}) {
+import styles from './SearchResults.module.css';
+
+export default function SearchResults({ tracks }) {
   return (
     <div>
       {tracks.map((track) => (
-        <div key={track.id}>
+        <div key={track.id} className={styles.trackCard}>
           <p>{track.name} by {track.artist}</p>
         </div>
       ))}
     </div>
   );
-  }
-  
+}
