bf2eb22ff53f1a87077d20ee875a516bd26c39ac
LS2603 <liam.snowball@eque2.com>
2025-04-20 09:52:56 +0100

    removed console logs for testing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { search } from './Spotify';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
@@ -5,6 +6,16 @@ import Playlist from './components/Playlist';
 import styles from './App.module.css';
 
 function App() {
+  useEffect(() => {
+    // Trigger token grab silently on load
+    try {
+      search(''); // Will not run a real search but will trigger getAccessToken
+    } catch (e) {
+      // If we threw an error to pause redirect, log it
+      console.log(e.message);
+    }
+  }, []);
+  
     const handleSearch = async (term) => {
     const results = await search(term);
     console.log('Spotify search results:', results);
--- src/Spotify.js
+++ src/Spotify.js
@@ -21,8 +21,7 @@ function getAccessToken() {
   // If no token, redirect user to Spotify login
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=${scope}&redirect_uri=${redirectUri}`;
   console.log('🔁 Would redirect to Spotify auth:', authUrl);
-// Pause everything before redirect
-throw new Error('⛔ Stopping redirect temporarily to inspect URL');
+  window.location = authUrl;
 }
 
 // Step 2: Use token to search Spotify
